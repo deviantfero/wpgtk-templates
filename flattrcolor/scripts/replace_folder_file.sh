aadfa0f1bbe9e16d69ea56d23ba8e2bd176bf0af
deviantfero <fmorataya.04@gmail.com>
2018-01-28 03:08:11 -0600

    update icon basefile

--- flattrcolor/scripts/replace_folder_file.sh
+++ flattrcolor/scripts/replace_folder_file.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 #	default color: 178984
-glyphColorOriginal=71744d
-glyphColorNew=524954
+glyphColorOriginal=294045
+glyphColorNew=444d25
 
 #	Front
 #	default color: 36d7b7
-frontColorOriginal=c4c886
-frontColorNew=9b8aa0
+frontColorOriginal=5f929c
+frontColorNew=99af54
 
 #	Back
 #	default color: 1ba39c
-backColorOriginal=959865
-backColorNew=716475
+backColorOriginal=355258
+backColorNew=56622f
 
 #	Paper
 #	default color: ffffff
 paperColorOriginal=ffffff
 paperColorNew=ffffff
 
+sed -i "s/#178984/#294045/g" $1
+sed -i "s/#9b8aa0/#5f929c/g" $1
+sed -i "s/#716475/#355258/g" $1
 sed -i "s/#$glyphColorOriginal;/#$glyphColorNew;/g" $1
 sed -i "s/#$frontColorOriginal;/#$frontColorNew;/g" $1
 sed -i "s/#$backColorOriginal;/#$backColorNew;/g" $1
-sed -i "s/#$paperColorOriginal;/#$paperColorNew;/g" $1
