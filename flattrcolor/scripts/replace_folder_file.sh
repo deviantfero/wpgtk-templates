020ed09dc13396b3fbac511813252abd1c2ad1eb
deviantfero <fmorataya.04@gmail.com>
2019-01-24 22:32:40 -0600

    update icon script and format

--- flattrcolor/scripts/replace_folder_file.sh
+++ flattrcolor/scripts/replace_folder_file.sh
@@ -1,22 +1,17 @@
 #!/usr/bin/env bash
 #	default color: 178984
-glyphColorOriginal=294045
-glyphColorNew=444d25
+oldglyph=#294045
+newglyph=#444d25
 
 #	Front
 #	default color: 36d7b7
-frontColorOriginal=5f929c
-frontColorNew=99af54
+oldfront=#5f929c
+newfront=#99af54
 
 #	Back
 #	default color: 1ba39c
-backColorOriginal=355258
-backColorNew=56622f
-
-#	Paper
-#	default color: ffffff
-paperColorOriginal=ffffff
-paperColorNew=ffffff
+oldback=#355258
+newback=#56622f
 
 sed -i "s/#178984/#294045/g" $1
 sed -i "s/#9b8aa0/#5f929c/g" $1
