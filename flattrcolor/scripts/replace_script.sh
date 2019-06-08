147ffa93de489c4ca52f41192e7d19ebf7be3d30
deviantfero <fmorataya.04@gmail.com>
2019-06-07 21:19:06 -0600

    update script to use .local/share

--- flattrcolor/scripts/replace_script.sh
+++ flattrcolor/scripts/replace_script.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
+
+if [ -n "${XDG_DATA_HOME}" ]; then
+  LOCAL="${XDG_DATA_HOME}"
+else
+  LOCAL="${HOME}/.local/share"
+fi
+
 function replaceall {
-	cd ~/.icons/flattrcolor/scripts
-	#sh ./replace_folder_file.sh change_folder_colors.xslt
+	cd "$LOCAL/icons/flattrcolor/scripts"
 	sh ./change_all_folders.sh
 }
 
